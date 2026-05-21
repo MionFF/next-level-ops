85e26c8c5a69895511daa5fd0bdf8af2a6041c5b
MionFF <deniskin741@gmail.com>
2026-05-21 14:35:41 +0300

    test: set up testing infrastructure

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/e2e/'],
+}
+
+export default createJestConfig(customJestConfig)
